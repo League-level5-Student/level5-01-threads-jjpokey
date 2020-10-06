c5b137cc3dd2436aa8a704277a99a797ee5c17d3
jjpokey <jasonelliott99@yahoo.com>
2020-10-05 17:00:03 -0700

    ThreadPool

--- src/_04_Thread_Pool/ThreadPool.java
+++ src/_04_Thread_Pool/ThreadPool.java
@@ -1,5 +1,39 @@
 package _04_Thread_Pool;
 
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 public class ThreadPool {
+Thread[] ta;
+ConcurrentLinkedQueue<Task> taskQueue;
+	public ThreadPool(int totalT) {
+		ta = new Thread[totalT];
+		
+		taskQueue = new ConcurrentLinkedQueue<Task>();
+		
+		for(int j = 0; j < ta.length; j++) {
+			ta[j] = new Thread(new Worker(taskQueue));
+		}
+	}
+
+	public void addTask(Task t) {
+		taskQueue.add(t);
+	}
+
+	public void start() {
+		
+		for(int j = 0; j < ta.length; j++) {
+			ta[j].start();
+		}
+		for(int a = 0; a < ta.length; a++) {
+			for(int j = 0; j < ta.length; j++) {
+				try {
+					ta[j].join();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+	}
 
 }
--- src/_04_Thread_Pool/Worker.java
+++ src/_04_Thread_Pool/Worker.java
@@ -1,11 +1,19 @@
 package _04_Thread_Pool;
 
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class Worker implements Runnable {
-
+	ConcurrentLinkedQueue<Task> taskQueue;
+	
+	public Worker(ConcurrentLinkedQueue taskQueue) {
+		this.taskQueue = taskQueue;
+		taskQueue = new ConcurrentLinkedQueue<Task>();
+	}
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+while(taskQueue != null) {
+	taskQueue.remove().perform();
+}
 		
 	}
 
